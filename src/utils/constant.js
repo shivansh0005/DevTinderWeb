f7830286f0f259947a280063c2e602deebcc425f
Your Name <shivansh chaturvedi>
2025-05-21 03:52:02 +0530

    Bug fix

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,6 @@
 export const BASE_URL = "http://localhost:3000";
 // export const BASE_URL = "https://devtinder-2-906v.onrender.com";
 
+// export const BASE_URL = "https://devtinder-3-w2yp.onrender.com";
+
 
