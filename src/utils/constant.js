af5114eec12c29b114e3aebaedaa9e4add324dea
Your Name <shivansh chaturvedi>
2025-05-25 00:21:32 +0530

    yo

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,7 @@
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
 // export const BASE_URL = "https://devtinder-2-906v.onrender.com";
 
 // export const BASE_URL = "https://devtinder-3-w2yp.onrender.com";
 
 
-// export const BASE_URL = "/api";
+export const BASE_URL = "/api";
