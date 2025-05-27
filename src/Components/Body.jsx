f756e996593cf45765b307f6b2541650cc04cf41
Your Name <shivansh chaturvedi>
2025-05-27 15:33:49 +0530

    "Footer overlap issue resolved"

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,4 +1,47 @@
-import React, { use } from 'react'
+// import React, { use } from 'react'
+// import NavBar from './NavBar'
+// import { useNavigate, Outlet } from 'react-router-dom'
+// import Footer from './Footer'
+// import axios from 'axios'
+// import { BASE_URL } from '../utils/constant'
+// import { addUser } from '../utils/userSlice'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { useEffect } from 'react'
+// const Body = () => {
+//   const  dispatch = useDispatch();
+//   const  navigate=useNavigate();
+//   const userData=useSelector((store)=>store.user);
+
+//   const fetchUser=async()=>{
+//     if(userData)return ;
+//     try{
+//     const user=await axios.get(BASE_URL+"/profile/view",{withCredentials:true});
+// dispatch(addUser(user.data));
+//   }
+//   catch(err){
+//     if(!userData){
+//       navigate("/login")
+//     }
+   
+  
+//   }
+    
+//   }
+
+//   useEffect(()=>{
+//   fetchUser();
+//   },[])
+//   return (
+//     <div>
+//       <NavBar/>
+//       <Outlet/>
+//       <Footer/>
+//     </div>
+//   )
+// }
+
+// export default Body
+import React from 'react'
 import NavBar from './NavBar'
 import { useNavigate, Outlet } from 'react-router-dom'
 import Footer from './Footer'
@@ -7,35 +50,37 @@ import { BASE_URL } from '../utils/constant'
 import { addUser } from '../utils/userSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
+
 const Body = () => {
-  const  dispatch = useDispatch();
-  const  navigate=useNavigate();
-  const userData=useSelector((store)=>store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
 
-  const fetchUser=async()=>{
-    if(userData)return ;
-    try{
-    const user=await axios.get(BASE_URL+"/profile/view",{withCredentials:true});
-dispatch(addUser(user.data));
-  }
-  catch(err){
-    if(!userData){
-      navigate("/login")
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const user = await axios.get(BASE_URL + "/profile/view", { withCredentials: true });
+      dispatch(addUser(user.data));
     }
-   
-  
-  }
-    
+    catch (err) {
+      if (!userData) {
+        navigate("/login")
+      }
+    }
   }
 
-  useEffect(()=>{
-  fetchUser();
-  },[])
+  useEffect(() => {
+    fetchUser();
+    // eslint-disable-next-line
+  }, [])
+
   return (
-    <div>
-      <NavBar/>
-      <Outlet/>
-      <Footer/>
+    <div className="flex flex-col min-h-screen bg-base-100">
+      <NavBar />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   )
 }
